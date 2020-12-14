db3b666c643848a32af48580e62269e42f98a747
Pankaj Mandrawal <45424590+pankajm05@users.noreply.github.com>
2020-12-14 20:24:31 +0530

    Create FarManager.cpp

---
+++ Templates/FarManager.cpp
@@ -0,0 +1,14 @@
+/** 
+ *    author:  pankaj_m05
+ *    created: @@DATETIME:dd.MM.yyyy HH\:mm\:ss@@
+**/ 
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  @@HERE@@
+  return 0;
+}
