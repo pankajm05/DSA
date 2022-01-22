6296d6983711c645297e4598c497e7a671d43fb3
pankajm05 <cse.18bcs2440@gmail.com>
2022-01-22 20:24:11 +0530

    Renamed files and added comments

--- Dynamic Programming/0-1 Knapsack/simple.cpp
+++ Dynamic Programming/0-1 Knapsack/simple.cpp
@@ -1,6 +1,14 @@
+/**
+ * cap: Knapsack Capacity
+ * w: weight of each item
+ * v: values (profit) of each item
+ * dp[i][j]: maximum possible profit using i items if the capacity of the knapsack was j
+ * Time Complexity: O(n^2)
+ * Space Complexity: O(n^2)
+**/
 template<typename T>
-T solve(int n, int cap, vector<T>& v, vector<T>& w) {
-  vector<vector<T>> dp(n + 1, vector<T>(cap + 1, (T) 0));
+T solve(int n, T cap, vector<T>& v, vector<T>& w) {
+  vector<vector<T>> dp(n + 1, vector<T>(cap + 1, T()));
   for (int i = 1; i <= n; ++i) {
     for (int j = 1; j <= cap; ++j) {
       if (w[i - 1] <= j) {
