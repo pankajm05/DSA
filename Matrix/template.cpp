4819e978a4833b0ef1abe78b16cf8feb1bdd81c1
Pankaj <cse.18bcs2440@gmail.com>
2022-04-05 13:56:32 +0530

    Fixed Bugs

--- Matrix/template.cpp
+++ Matrix/template.cpp
@@ -1,29 +1,33 @@
 template<typename T>
 class matrix {
 public:
-  const T identity = T(1);
   int n, m;
   vector<vector<T>> mat;
-  matrix(int _n, int _m, T v = T()) : n(_n), m(_m) {
+  matrix(int _n, int _m, T v = T(0)) : n(_n), m(_m) {
     mat.resize(n);
     for (int i = 0; i < n; i++) {
       mat[i].assign(m, v);
     }
   }
 
-  matrix(vector<vector<T>> &a) {
+  explicit matrix(vector<vector<T>> &a) : n(a.size()), m(a[0].size()) {
     assert(!a.empty());
-    assert(!a[0].empty());
-    matrix<T>(a.size(), a[0].size());
+    assert(!a[0].empty());           
+    mat.resize(n);
     for (int i = 0; i < n; i++) {
+      mat[i].resize(m);
       for (int j = 0; j < m; j++) {
         mat[i][j] = a[i][j];
       }
     }
   }
 
-  matrix<T> operator [](const int id) {
-    return mat[id];
+  matrix<T> identity() {
+    matrix<T> ans(n, m);
+    for (int i = 0; i < n; i++) {
+      ans.mat[i][i] = T(1);
+    }
+    return ans;
   }
 
   matrix<T> operator +(const matrix<T> &other) {
@@ -52,22 +56,12 @@ public:
 
   matrix<T> operator *(const matrix<T> &other) {
     assert(m == other.n);
-    matrix<T> ans(n, other.m);
+    matrix<T> ans(n, other.m, T(0));
     for (int i = 0; i < ans.n; i++) {
       for (int j = 0; j < ans.m; j++) {
         for (int k = 0; k < m; k++) {
-          ans.mat[i][j] += (mat[i][k] * other.mat[k][j]);
+          ans.mat[i][j] = ans.mat[i][j] + (mat[i][k] * other.mat[k][j]);
         }
-      }
-    }
-    return ans;
-  }
-
-  matrix<T> operator *(const T scaler) {
-    matrix<T> ans(mat);
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        ans.mat[i][j] *= scaler;
       }
     }
     return ans;
@@ -76,13 +70,13 @@ public:
 
 template<class T>
 matrix<T> mat_power(matrix<T> &a, long long b) {
-  matrix<T> ans(a.n, a.m, a.identity);
-  while (b) {
-    if (b % 2 == 1) {
+  matrix<T> ans = a.identity();
+  while (b > 0) {
+   if (b % 2 == 1) {
       ans = ans * a;
     }
     a = a * a;
     b >>= 1;
-  }
+  }            
   return ans;
 }
